50915bca248c675634a12b23166877c18bdd6958
John Schimansky <schimmy@gmail.com>
2020-02-12 19:31:59 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,12 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  new_array = array.each do |word|
-    word.reverse!
-  end
-return new_array.join(" ")
+  reverse_array = []
+  array.each { |word| reverse_array << word.reverse}
+  reverse_array.join(" ")
 end
 
 def reverse_each_word(string)
   array = string.split(" ")
-  new_array = array.collect do |word|
-    word.reverse
-  end
-  new_array.join(" ")
+  array = array.collect { |word| word.reverse}
+  array.join(" ")
 end
